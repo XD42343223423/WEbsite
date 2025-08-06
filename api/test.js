230d377dad1fe9e2e372e41d8ca0e7c2e1c9b6c0
=XD= <89638937+XD42343223423@users.noreply.github.com>
2025-08-05 20:55:13 -0400

    Delete api/test.js

--- api/test.js
+++
@@ -1,9 +0,0 @@
-import formidable from 'formidable';
-
-export const config = {
-  api: { bodyParser: false }
-};
-
-export default async function handler(req, res) {
-  return res.status(200).json({ status: 'Formidable is working!' });
-}
